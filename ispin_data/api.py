3084b9edc7e14ef667e4d66a0c8885041f8db948
rodrigo087 <89451777+rodrigo087@users.noreply.github.com>
2021-08-24 13:03:46 +0200

    Add files via upload

--- ispin_data/api.py
+++ ispin_data/api.py
@@ -1,22 +1,26 @@
+# Module import
 import numpy as np
 
-class Data:
-    """Get iSpin data for a specified turbine ID"""
+#    def __init__(self, case_sensitive=False):
+#
+#        """Creates a detector parsing given data file"""
+#        self.case_sensitive = case_sensitive
 
-    def __init__(self, case_sensitive=False):
+def __circle_area(ID):
+    """ 
+    Finds the area of a circle fiven by the specified turbine ID
+    """
+    a = round(np.pi * ID**2)
+    return a
 
-        """Creates a detector parsing given data file"""
-        self.case_sensitive = case_sensitive
+def request_data(ID):
+    """
+    Returns iSpin data for a specified turbine ID
+    """
 
-    def _circle_area(self, ID):
-        """Finds the area of a circle fiven by the specified turbine ID"""
-        a = round(np.pi * ID**2)
-        return a
+    if type(ID) == int:
+        return __circle_area(ID)
+    else:
+        raise Exception("The provided turbine ID does not exists")
 
-    def request_data(self, ID):
-        """Returns iSpin data for a specified turbine ID"""
 
-        if type(ID) == int:
-            return self._circle_area(self, ID)
-        else:
-            raise Exception("The provided turbine ID does not exists")
