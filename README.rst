881becda257422cf937d3f06aab530a518e4e008
rodrigo087 <89451777+rodrigo087@users.noreply.github.com>
2021-08-24 11:29:55 +0200

    Add files via upload
    Upload files

---
+++ README.rst
@@ -0,0 +1,36 @@
+==============
+iSpin Data
+==============
+
+.. image:: https://travis-ci.org/lead-ratings/gender-guesser.svg?branch=master
+    :target: https://travis-ci.org/lead-ratings/gender-guesser_XXX
+
+
+This package provides access to iSpin data for a specified turbine ID.  Its use is pretty straightforward::
+
+    >>> import ispin_data.api as ispin
+    >>> iSpin = ispin.Data()
+    >>> df = iSpin.request_data(34)
+    3634
+    >>> df = iSpin.request_data(6)
+    113
+
+The explanation each columns of the returned DataFrame can be consulted in the XXXXXXXX.txt file
+
+For now, the function will only provide the area of a circle given by the specified turbine ID
+
+
+Licenses
+========
+
+Nabla Wind Hub.
+
+
+Changelog
+=========
+
+0.1.0 (2021-08-24)
+******************
+
+* First release of the package
+
---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+from io import open
+import sys
+
+
+class Tox(TestCommand):
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import tox
+        import shlex
+        args = self.tox_args
+        if args:
+            args = shlex.split(self.tox_args)
+        errno = tox.cmdline(args=args)
+        sys.exit(errno)
+
+setup(
+    name='iSpin-data',
+    version='0.1.0',
+    author='Rodrigo Hurtado',
+    author_email='rel@romowind.com',
+    packages=['ispin_data'],
+    package_dir={'ispin_data': 'ispin_data'},
+    url='https://github.com/lead-ratings/gender-guesser',
+    description='Gets iSpin data for a turbine ID.',
+    long_description=open('README.rst', encoding='utf-8').read(),
+)
