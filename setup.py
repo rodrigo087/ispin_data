912ad608b2c436adf6e44e867c6f5fb5ae2a6dc3
rodrigo087 <89451777+rodrigo087@users.noreply.github.com>
2021-08-25 13:10:01 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ class Tox(TestCommand):
 
 setup(
     name='iSpin-data',
-    version='0.1.6',
+    version='0.1.7',
     author='Rodrigo Hurtado',
     author_email='rel@romowind.com',
     packages=['ispin_data'],
