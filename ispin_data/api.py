889baa49b45059cb17127fc5bbaa28a4e8a2d514
rodrigo087 <89451777+rodrigo087@users.noreply.github.com>
2021-08-25 12:46:35 +0200

    Add files via upload

--- ispin_data/api.py
+++ ispin_data/api.py
@@ -3,14 +3,8 @@ username = None
 password = None
 
 
+
 # Functions
-#def __init__(self, case_sensitive=False):
-#
-#    """Creates a detector parsing given data file"""
-#    self.case_sensitive = case_sensitive
-
-
-
 def __authorize():
     """
     User authentication
@@ -29,8 +23,6 @@ def __authorize():
     def post_token():      
         __baseUrl = "https://dc.romowind.net/api"
         response = requests.post(__baseUrl + "/token", data=parameters, verify=False)
-        if (response.status_code == 200):
-            print("*** Authenticated ***")
         return response
 
     if (username==None) & (password==None):
@@ -74,41 +66,57 @@ def request_overview():
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def __circle_area_calc(ID):
-    """ 
-    Finds the area of a circle fiven by the specified turbine ID
+def request_iSpin_data(turb, start_date=None, end_date=None):
     """
-    import numpy as np
-    a = round(np.pi * ID**2)
-    return a
+    Loads iSpin data for a turbine ID through the API
+    
+    Parameters
+    --------------
+    turb : int
+        Turbine ID for the data to be loaded (see "request_overview()")
+    start_date : str
+        staring date of the data to be loaded in the format: 'YYYY-MM-DD' or 'YYYY-MM-DD HH:MM:SS'
+    end_date : str
+        ending date of the data te be loaded in the format: 'YYYY-MM-DD' or 'YYYY-MM-DD HH:MM:SS'
+    """  
+    
+    import pandas as pd
+    import requests
+    import datetime
+    
+    # start and end date format
+    if type(turb) != int:
+        raise Exception('Please set the turbine ID as an integer')
+    if (start_date != None) | (end_date != None):
+        if (type(start_date) != str) or (type(end_date) != str):
+            raise Exception('Please state the start and the end dates as strings in the format: "YYYY-MM-DD" or "YYYY-MM-DD HH:MM:SS"')
+    
+    # Get all data if the start and end date are not specified
+    if start_date == None:
+        start_date = '2010-01-01'
+    if end_date == None:
+        end_date = str(datetime.datetime.now())[0:10]
+    
+    # Obtaining authentication token
+    authHeader = __authorize()
+    
+    # Requesting the data
+    __baseUrl = "https://dc.romowind.net/api"
+    URL = __baseUrl + '/turbines/%d/data?fromTime=%s&toTime=%s' % (turb, start_date, end_date)
+    response = requests.get(URL, headers=authHeader, verify=False)
+    Dataresponse = response.json()
+    
+    # Formatting the output DataFrame
+    df = pd.DataFrame(Dataresponse)
+    df.sampleTime = pd.to_datetime(df.sampleTime)
+    df = df.set_index('sampleTime')
+    df.index = df.index.tz_localize(None)
+       
+    # Sorting the columns
+    df = df.drop(['metDataValid', 'windVaneAdjustmentIndex'], axis=1)
+    df = df.reindex(sorted(df.columns), axis=1)
+    
+    return df
 
-def circle_area(ID):
-    """
-    Returns iSpin data for a specified turbine ID
-    """
-
-    if type(ID) == int:
-        return __circle_area_calc(ID)
-    else:
-        raise Exception("The provided turbine ID does not exists")
 
 
